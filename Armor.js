6e8b5f97d1ca4e14db7e9bf9c3408c580a692597
PS <pavelsotnik92@gmail.com>
2020-08-01 11:35:07 +0300

    2

--- Armor.js
+++ Armor.js
@@ -1,5 +1,4 @@
 var
-armor_config =
-[
-    {title: 'New Moon Acolyte ', type: 'light'/*, set2: weaponCritical+833 spellCritical+833, set3: weaponDamage+129, spellDamage+129, set4: physicalPenetration+1487 spellPenetration+1487, set5: weaponDamage+481 spellDamage+481, Unique:'Increases the cost of your active abilities by 5%.*/}
+armor_config =[
+    {title: 'New Moon Acolyte ', type: 'light', set2: weaponCritical+833 spellCritical+833, set3: weaponDamage+129, spellDamage+129, set4: physicalPenetration+1487 spellPenetration+1487, set5: weaponDamage+481 spellDamage+481, Unique:'Increases the cost of your active abilities by 5%.'}
 ]
