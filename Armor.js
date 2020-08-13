4597eb2218b63d499a2fdb65779259b07c5d9b65
PS <pavelsotnik92@gmail.com>
2020-08-13 16:23:50 +0300

    Search

--- Armor.js
+++ Armor.js
@@ -1,4 +1,40 @@
 var
-armor_config =[
-    {title: 'New Moon Acolyte ', type: 'light', set2: weaponCritical+833 spellCritical+833, set3: weaponDamage+129, spellDamage+129, set4: physicalPenetration+1487 spellPenetration+1487, set5: weaponDamage+481 spellDamage+481, Unique:'Increases the cost of your active abilities by 5%.'}
+armor_config = [
+    {title: 'light New Moon Acolyte Helmet ', part: 'Helmet', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'light New Moon Acolyte Shoulder', part: 'Shoulder', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'light New Moon Acolyte Chest', part: 'Top', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'light New Moon Acolyte Legs', part: 'Bottom', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'light New Moon Acolyte Waist', part: 'Sash', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'light New Moon Acolyte Hands', part: 'Gloves', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'light New Moon Acolyte Feet', part: 'Boots', type: 'light', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+
+    {title: 'Medium New Moon Acolyte Helmet ', part: 'Helmet', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Medium New Moon Acolyte Shoulder', part: 'Shoulder', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Medium New Moon Acolyte Chest', part: 'Top', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Medium New Moon Acolyte Legs', part: 'Bottom', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Medium New Moon Acolyte Waist', part: 'Sash', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Medium New Moon Acolyte Hands', part: 'Gloves', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Medium New Moon Acolyte Feet', part: 'Boots', type: 'Medium', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+
+    {title: 'Heavy New Moon Acolyte Helmet ', part: 'Helmet', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Heavy New Moon Acolyte Shoulder', part: 'Shoulder', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Heavy New Moon Acolyte Chest', part: 'Top', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Heavy New Moon Acolyte Legs', part: 'Bottom', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Heavy New Moon Acolyte Waist', part: 'Sash', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Heavy New Moon Acolyte Hands', part: 'Gloves', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'},
+    {title: 'Heavy New Moon Acolyte Feet', part: 'Boots', type: 'Heavy', set2: 'weaponCritical+833, spellCritical+833', set3: 'weaponDamage+129, spellDamage+129', set4: 'physicalPenetration+1487 spellPenetration+1487', set5:'weaponDamage+481 spellDamage+481', Unique:'Increases the cost of your active abilities by 5%.'}
 ]
+
+var armor_elements = []
+
+var dropdown_items = document.querySelector('.dropdown-items')
+armor_config.forEach(function(item) {
+    var EL = document.createElement('div')
+    EL.className = 'dropdown_item'
+    EL.innerHTML = item.title
+    dropdown_items.appendChild(EL)
+
+    armor_elements.push(El)
+})
+
+
